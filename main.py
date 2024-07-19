f90d33507d124d9992e038d8788492254bf60452
Listoker <151549199+Listoker@users.noreply.github.com>
2024-07-19 18:35:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import os
 import datetime
 from flask import Flask, redirect, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, FloatField , BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired
 from data import db_session
 import sqlalchemy
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+
+
+class MyForm(FlaskForm):
+    name = FloatField('A', validators=[InputRequired()])
 
 @app.route('/')
 def glavnoe():
@@ -20,7 +26,7 @@ def glavnoe():
 def assortiment(tovar):
     # ассортимент
     with open(f'assortiment/{tovar}.txt', encoding='UTF-8') as f:
-        text_vsego = f.read().split('#@{]')
+        text_vsego = f.read().split('#')
         return render_template('assortiment.html', tovari=text_vsego)
 
 
@@ -28,6 +34,16 @@ def assortiment(tovar):
 def kontakti():
     # контакты
     return render_template('kontakti.html')
+
+
+@app.route('/calculator', methods=['GET', 'POST'])
+def calculator():
+    form = MyForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        print(name)
+        return render_template('calculator.html', form=form)
+    return render_template('calculator.html', form=form)
 
 
 @app.route('/tovar/<beton>')
